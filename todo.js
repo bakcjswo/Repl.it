7940a7957a4dff276acc80963cd22f67c625db82
bakcjswo <59162031+bakcjswo@users.noreply.github.com>
2020-01-04 15:12:29 +0900

    1/4/2019 3:12 PM

---
+++ todo.js
@@ -0,0 +1,72 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  li.classList.add("delToDoAnim");
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.className = "js-delBtn";
+  const div = document.createElement("div");
+  div.className = "js-toDoList__ToDoText";
+  const newId = toDos.length + 1;
+  delBtn.innerText = "X";
+  delBtn.addEventListener("click", deleteToDo);
+  div.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(div);
+  li.id = newId;
+  li.className = "js-toDoList__toDoList";
+  toDoList.appendChild(li);
+  li.classList.add("slide-bck-center-out");
+  li.classList.remove("slide-bck-center-out");
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function something(toDo) {
+      paintToDo(toDo.text);
+    });
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
